e687478203cc2bb5dcac6c6cd022f0b46b46be0d
EC2 Default User <ec2-user@ip-172-31-39-164.ec2.internal>
2021-11-14 06:50:02 +0000

    question投稿まで

--- app/controllers/public/questions_controller.rb
+++ app/controllers/public/questions_controller.rb
@@ -1,25 +1,32 @@
 class Public::QuestionsController < ApplicationController
-  
+
   def new
-  
+    @question = Question.new
   end
-  
+
   def index
-  
+
   end
-  
+
   def show
-  
+    @question = Question.find(params[:id])
   end
-  
+
   def create
-  
+    @question = Question.new(question_params)
+    @question.user_id = current_user.id
+    @question.save
+    redirect_to public_question_path(@question.id)
   end
-  
+
   def destroy
-    
+
   end
-  
-  
-  
+
+  private
+
+  def question_params
+   params.require(:question).permit(:user_id, :genre_id, :question_status_id, :title, :body)
+  end
+
 end
